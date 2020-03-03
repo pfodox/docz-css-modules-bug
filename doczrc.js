1ca1b546981317d3207318533b7f40e35c67eb31
a.mikhailov <a.mikhailov@cian.ru>
2020-03-03 21:59:48 +0300

    Css modules bug

---
+++ doczrc.js
@@ -0,0 +1,4 @@
+export default {
+  typescript:true,
+  files: "./src/**/*.mdx",
+}
---
+++ src/Button.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as styles from './styles.css';
+
+export const Button = () => {
+  return (
+    <button className={styles['button']}>my button</button>
+  )
+};
